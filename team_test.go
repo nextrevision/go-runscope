d8820dd249eabbf0ebe6a304c744dbb1ba96bf02
nextrevision <notarobot@nextrevision.net>
2016-04-26 10:38:47 -0400

    Adding list people method

---
+++ team_test.go
@@ -0,0 +1,44 @@
+package runscope
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestListPeople(t *testing.T) {
+	setup()
+	defer teardown()
+
+	path := "/teams/1/people"
+	responseCode := http.StatusOK
+	responseData := `
+{
+  "data": [
+    {
+      "email": "grace@example.com",
+      "name": "Grace Hopper",
+      "uuid": "cf95026e-8951-4ae1-83a7-699243678490"
+    }
+  ],
+  "error": null,
+  "meta": {
+    "status": "success"
+  }
+}`
+	want := &[]Person{
+		Person{
+			UUID:  "cf95026e-8951-4ae1-83a7-699243678490",
+			Name:  "Grace Hopper",
+			Email: "grace@example.com",
+		},
+	}
+
+	handleGet(t, path, responseCode, responseData)
+
+	result, resp, err := client.ListPeople("1")
+	testStatusCode(t, resp, responseCode)
+	if err != nil {
+		t.Errorf("ListPeople returned error: %v", err)
+	}
+	testResponseData(t, result, want)
+}
